1bdc38be94584b3b0065733927fd47e43e13e0db
tehpunisher456 <55720542+tehpunisher456@users.noreply.github.com>
2020-01-04 13:42:00 -0800

    further updated layout for front page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.carousel').carousel();
+    autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
   });
